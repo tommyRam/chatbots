49ef7365edb52149355e07c280ef9888629b8e8d
Tommy Ram <nomenaramarokoto28@gmail.com>
2025-06-21 12:22:32 +0200

    fix: remove async in crud action because it is already managed by sqlalchemy

--- backend/app/router/api.py
+++ backend/app/router/api.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Depends
 import sqlalchemy.orm as orm
+from fastapi.security import OAuth2PasswordRequestForm
 
 from .schemas import TokenResponse, RegisterUserRequest, LoginUserRequest, UserResponse, UserSchema
 from .crud import get_user_by_email, get_user_by_username
@@ -16,36 +17,32 @@ async def register_user(
     payload: RegisterUserRequest,
     db: orm.Session = Depends(get_db)
 ):
-    user_by_username = await get_user_by_username(payload.username, db)
+    user_by_username = get_user_by_username(payload.username, db)
     if user_by_username: 
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Username already exist. Please try another one."
         )
 
-    user_by_email = await get_user_by_email(payload.email, db)
+    user_by_email = get_user_by_email(payload.email, db)
     if user_by_email:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="The email already used. Please try another one."
         )
     
-    user_model = await create_user(payload, db)
-    print("================== user_model", user_model)
+    user_model = create_user(payload, db)
     user_schema = UserSchema.model_validate(user_model)
 
     user_data = user_schema.model_dump(mode="json")
-    access_token = await create_access_token(user_data)
-    refresh_token = await create_refresh_token(user_model.id, db)
+    access_token = create_access_token(user_data)
+    refresh_token = create_refresh_token(user_model.id, db)
 
     return TokenResponse(
         access_token=access_token,
         refresh_token=refresh_token,
         expires_in=settings.REFRESH_TOKEN_EXPIRE_DAYS
     )
-
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordRequestForm
 
 @router.post("/auth/login", response_model=TokenResponse)
 async def login_user(
@@ -55,7 +52,7 @@ async def login_user(
     user_identifier = form_data.username
     password = form_data.password
 
-    is_user = await is_user_exist(user_identifier, db)
+    is_user = is_user_exist(user_identifier, db)
 
     if not is_user:
         raise HTTPException(
@@ -63,8 +60,8 @@ async def login_user(
             detail="The username or email doesn't exist. Please try again"
         )
 
-    user_by_email = await get_user_by_email(user_identifier, db)
-    user_by_username = await get_user_by_username(user_identifier, db)
+    user_by_email = get_user_by_email(user_identifier, db)
+    user_by_username = get_user_by_username(user_identifier, db)
     user = user_by_email if user_by_email else user_by_username
 
     password_is_ok = verify_password(password, user.hashed_password)
@@ -76,8 +73,8 @@ async def login_user(
 
     user_schema = UserSchema.model_validate(user)
     user_data = user_schema.model_dump(mode="json")
-    access_token = await create_access_token(user_data)
-    refresh_token = await create_refresh_token(user.id, db)
+    access_token = create_access_token(user_data)
+    refresh_token = create_refresh_token(user.id, db)
 
     print("response: ", access_token)
 
--- backend/app/router/crud.py
+++ backend/app/router/crud.py
@@ -3,32 +3,32 @@ from datetime import datetime
 
 from .models import UserModel, RefreshTokenModel
 
-async def get_user_by_email(email: str, db: orm.Session):
+def get_user_by_email(email: str, db: orm.Session):
     return db.query(UserModel).filter(UserModel.email == email).first()
 
-async def get_user_by_id(id: str, db: orm.Session):
+def get_user_by_id(id: str, db: orm.Session):
     return db.query(UserModel).filter(UserModel.id == id).first()
 
-async def get_user_by_username(username: str, db: orm.Session):
+def get_user_by_username(username: str, db: orm.Session):
     return db.query(UserModel).filter(UserModel.username == username).first()
 
-async def add_user(user: UserModel, db: orm.Session):
+def add_user(user: UserModel, db: orm.Session):
     db.add(user)
     db.commit()
     db.refresh(user)
 
-async def add_refresh_token(refresh_token: RefreshTokenModel, db: orm.Session):
+def add_refresh_token(refresh_token: RefreshTokenModel, db: orm.Session):
     db.add(refresh_token)
     db.commit()
 
-async def get_refresh_token_by_token(refresh_token: str, db: orm.Session):
+def get_refresh_token_by_token(refresh_token: str, db: orm.Session):
     return db.query(RefreshTokenModel).filter(
         RefreshTokenModel.token == refresh_token,
         RefreshTokenModel.is_revoked == False,
         RefreshTokenModel.expires_at > datetime.now()
     ).first()
 
-async def get_active_refresh_token_from_user_id(user_id: str, db: orm.Session):
+def get_active_refresh_token_from_user_id(user_id: str, db: orm.Session):
     return db.query(RefreshTokenModel).filter(
         RefreshTokenModel.user_id == user_id,
         RefreshTokenModel.is_revoked == False
